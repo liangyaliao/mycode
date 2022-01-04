8e27dfd521532f58882d3d665a61c7d56aea9335
Liangyan Liao <liaoliangyan@gmail.com>
2022-01-04 22:20:03 +0000

    your commit message

---
+++ challenge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+icecream=["indentation", "spaces"]
+
+tlgstudents= ["Aaron","Alex","Alonzo","Brandon","Chris","Francisco","James","Jonathan","Lillian","Manuel","Patrick","Robert","Ryan","Troy","Wes","Henry","Yalined"]
+
+icecream.append(4)
+
+choice= input(input("Pick a student number!"))
+
+student_name= tlgstudents[choice]
+
+
+print("student_name)
+
